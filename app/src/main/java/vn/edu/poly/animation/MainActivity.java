bbfd6ed3c154fad8d1eb7f7ab7f5ab982705a502
HuyNguyen <huuhuybn@gmail.com>
2019-02-21 22:19:37 +0700

    update demo

--- app/src/main/java/vn/edu/poly/animation/MainActivity.java
+++ app/src/main/java/vn/edu/poly/animation/MainActivity.java
@@ -4,110 +4,113 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgObj;
+
+    private SharedPreferences sharedPreferences;
+    private TextView tvText;
+    private Button btnLoad;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgObj = findViewById(R.id.imgObj);
+
+        tvText = findViewById(R.id.tvText);
+        btnLoad = findViewById(R.id.btnLoad);
+        btnSave = findViewById(R.id.btnSave);
 
 
-
-        // Property Animation
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(imgObj,
-                "rotation", 0, 500);
-
-        objectAnimator.setDuration(5000);
-        objectAnimator.setRepeatCount(5);
-
-        // di chuyen lien tuc 5 lan va dao nguoc
-        objectAnimator.setRepeatMode(ObjectAnimator.REVERSE);
-
-        objectAnimator.addListener(new AnimatorListenerAdapter() {
+        btnLoad.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAnimationCancel(Animator animation) {
-                super.onAnimationCancel(animation);
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                super.onAnimationRepeat(animation);
-            }
-
-            @Override
-            public void onAnimationStart(Animator animation) {
-                super.onAnimationStart(animation);
-            }
-
-            @Override
-            public void onAnimationPause(Animator animation) {
-                super.onAnimationPause(animation);
-            }
-
-            @Override
-            public void onAnimationResume(Animator animation) {
-                super.onAnimationResume(animation);
+            public void onClick(View v) {
+                loadListCache();
             }
         });
-        //objectAnimator.start();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createFileCache();
+            }
+        });
+
+        sharedPreferences = getSharedPreferences("DATA", MODE_PRIVATE);
+
+        // save du lieu
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("NAME", "HUY NGUYEN");
+
+        editor.apply();
 
 
+        // load du lieu ra
 
-        Animator animator = AnimatorInflater.loadAnimator(this,
-                R.animator.my_animation);
+        String name = sharedPreferences.getString("NAME", "CHUA LUU");
+
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
 
 
-        animator.setTarget(imgObj);
+    }
 
-        animator.start();
+    public void createFileCache() {
 
-        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.woman_scream);
+        // lay ra duong dan cua cache file
+        File pathCacheDir = getCacheDir();
 
+        // dat ten file cache
+        String strCacheFileName = "myCacheFile" + new Random().nextInt() + ".cache";
+
+        // tao du lieu de luu
+        String strFileContents = "Hello World";
+
+        // khoi tao 1 file moi voi duong dan la cachefile va noi dung da khoi tao
+        File newCacheFile = new File(pathCacheDir, strCacheFileName);
         try {
-            mPlayer.start();
-        } catch (Exception e) {
-// handle this later
+            newCacheFile.createNewFile();
+            FileOutputStream foCache = new FileOutputStream(newCacheFile.getAbsolutePath());
+            foCache.write(strFileContents.getBytes());
+            foCache.close();
+            Toast.makeText(
+                    MainActivity.this, "Save successful!!!", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(
+                    MainActivity.this, "Somethings wrong!!!", Toast.LENGTH_LONG).show();
+
         }
 
 
+    }
 
-        // drawable animation
+    public void loadListCache() {
+        File pathFile = getCacheDir();
+        File[] listCache = pathFile.listFiles();
+        Toast.makeText(
+                MainActivity.this, listCache.length + "", Toast.LENGTH_LONG).show();
+        for (File file : listCache) {
 
-        imgObj.setImageResource(R.drawable.anabella);
-
-        AnimationDrawable animationDrawable =
-                (AnimationDrawable) imgObj.getDrawable();
-
-        animationDrawable.start();
-
-
-
-        // view animation
-        Animation hyperspaceJump = AnimationUtils.loadAnimation(
-                this, R.anim.slide_out);
-
-        imgObj.startAnimation(hyperspaceJump);
-
-
+        }
     }
 }
